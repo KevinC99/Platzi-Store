e777e7c43bb5dec593f83ffefe1703ef84039e6c
KevinC <kevincordovadavila99@gmail.com>
2021-11-30 07:52:37 -0500

    agregacion de admin en header

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 
-import { ExponentialPipe } from './pipes/exponential/exponential.pipe';
+import { ExponentialPipe } from './pipes/Exponential/exponential.pipe';
 import { HighlightDirective } from './directives/highlight/highlight.directive';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
