155a5ce593aa08aebe5c8d389a6b12a7cd79755b
KevinC <kevincordovadavila99@gmail.com>
2022-01-03 15:43:12 -0500

    avance hasta la clase 51 del curso que son el uso de pipes para contador dinamico de carrito

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { map, Observable } from 'rxjs';
+import { CartService } from 'src/app/core/Services/cart.service';
 
 @Component({
   selector: 'app-header',
@@ -6,8 +8,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
+  total$:Observable<number>;
 
-  constructor() { }
+  constructor(
+    private cartService:CartService,
+  ) {
+    this.total$=this.cartService.cart$
+    .pipe(
+      map(products=>products.length)
+    );
+   }
 
   ngOnInit() {
   }
