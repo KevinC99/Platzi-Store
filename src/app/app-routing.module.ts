79cadfebcf1b5ef732222046b4b4f0dc10704f78
KevinC <kevincordovadavila99@gmail.com>
2022-01-14 07:47:09 -0500

    dinmnio comenzando el login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,13 +7,21 @@ import { AdminGuard } from './admin.guard';
 
 const routes: Routes = [
   {
+      path: '',
+      redirectTo: 'login',
+      pathMatch: 'full',
+    },
+    {
+      path: 'login',
+      loadChildren: () => import('./login/login.module').then(m => m.LoginModule)
+    },
+    {
     path: '',
     component: LayoutComponent,
     children: [
       {
-        path: '',
-        redirectTo: '/home',
-        pathMatch: 'full',
+        path: 'login',
+        loadChildren: () => import('./login/login.module').then(m => m.LoginModule)
       },
       {
         path: 'home',
